7a2a2466580b926fb0d19b1c78bcd9009a110ac7
黄兵 <1278032416@qq.com>
2021-05-27 11:13:46 +0800

    add mail

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/go-rod/rod v0.99.0
 	github.com/robfig/cron/v3 v3.0.0
 	github.com/spf13/viper v1.7.1
+	github.com/xhit/go-simple-mail/v2 v2.9.1 // indirect
 )
--- message/mail.go
+++ message/mail.go
@@ -1,1 +1,70 @@
 package message
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/hb0730/auto-sign/config"
+	"github.com/hb0730/auto-sign/utils"
+	mail2 "github.com/xhit/go-simple-mail/v2"
+	"sync"
+)
+
+type Mail struct {
+	Host     string `json:"host" yaml:"host"`
+	Protocol string `json:"protocol" yaml:"protocol"`
+	Port     int    `json:"port" yaml:"port"`
+	Username string `json:"username" yaml:"username"`
+	Password string `json:"password" yaml:"password"`
+	FromName string `json:"from_name" yaml:"from_name"`
+	To       string `json:"to" yaml:"to"`
+}
+
+var mail Mail
+
+func init() {
+	utils.Info("read mail config ...")
+	yaml := config.ReadYaml()
+	mailMap := yaml.GetStringMap("message.mail")
+	bt, _ := json.Marshal(mailMap)
+	_ = json.Unmarshal(bt, &mail)
+	Register("mail", mail)
+}
+
+func (m Mail) Send(message MessageBody) {
+	eml := NewServer()
+	client, err := eml.Connect()
+	if err != nil {
+		utils.Error("create mail server error")
+		panic(err)
+	}
+	msg := mail2.NewMSG()
+	msg.SetFrom(setFrom(m.FromName, m.Username)).
+		AddTo(m.To).
+		SetSubject(message.Title).
+		SetBody(mail2.TextHTML, message.Content)
+	err = msg.Send(client)
+	defer client.Close()
+}
+
+var server *mail2.SMTPServer
+var mutex sync.Mutex
+
+// NewServer 创建 SMTPServer 服务
+func NewServer() *mail2.SMTPServer {
+	mutex.Lock()
+	defer mutex.Unlock()
+	if server == nil {
+		client := mail2.NewSMTPClient()
+		client.Host = mail.Host
+		client.Port = mail.Port
+		client.Password = mail.Password
+		client.Username = mail.Username
+		client.Encryption = mail2.EncryptionSSL
+		server = client
+	}
+	return server
+}
+
+func setFrom(from string, username string) string {
+	return fmt.Sprintf("From %s <%s>", from, username)
+}
